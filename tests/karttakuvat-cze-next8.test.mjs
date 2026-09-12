0f5c6c8a6d68e257f4e1ccaf930b27a74819e8a0
ravelius <sami@valokuvaamoklik.fi>
2026-09-12 17:56:26 +0300

    Correct CZE photo color profiles and derivative credits

--- tests/karttakuvat-cze-next8.test.mjs
+++ tests/karttakuvat-cze-next8.test.mjs
@@ -4,14 +4,14 @@ import { MAASTOKOHTEET_CZE } from '../js/packs/maastokohteet-cze.js';
 import { kohteenKuvalista } from '../js/fokuskohteet.js';
 
 const ODOTETUT = [
-  ['cesky-krumlov', 'cze-kohde-cesky-krumlov-0855c1b7e53b.jpg', 'cze-kohde-cesky-krumlov-93adb842e252.jpg'],
-  ['plzensky-prazdroj', 'cze-kohde-plzensky-prazdroj-5711ddfee90f.jpg', 'cze-kohde-plzensky-prazdroj-a4f9adc0995f.jpg'],
-  ['mendelin-luostari', 'cze-kohde-mendelin-luostari-56a42eb73e55.jpg', 'cze-kohde-mendelin-luostari-c9bb1706dad9.jpg'],
-  ['litomysl', 'cze-kohde-litomysl-91c788e7564e.jpg', 'cze-kohde-litomysl-763c119efc7f.jpg'],
+  ['cesky-krumlov', 'cze-kohde-cesky-krumlov-0855c1b7e53b.jpg', 'cze-kohde-cesky-krumlov-0504b4128cac.jpg', 'Vincent de Groot'],
+  ['plzensky-prazdroj', 'cze-kohde-plzensky-prazdroj-5711ddfee90f.jpg', 'cze-kohde-plzensky-prazdroj-3fa635c8f728.jpg', 'David J. Fred (Dfred)'],
+  ['mendelin-luostari', 'cze-kohde-mendelin-luostari-56a42eb73e55.jpg', 'cze-kohde-mendelin-luostari-59300d2a69c1.jpg', 'Billsofer'],
+  ['litomysl', 'cze-kohde-litomysl-91c788e7564e.jpg', 'cze-kohde-litomysl-3bc6ed892381.jpg', 'Twisp'],
 ];
 
 test('neljän CZE-kohteen karusellissa havainne on ennen aitoa valokuvaa', () => {
-  for (const [id, havainne, valokuva] of ODOTETUT) {
+  for (const [id, havainne, valokuva, alkuperaistekija] of ODOTETUT) {
     const kohde = MAASTOKOHTEET_CZE.find((x) => x.id === id);
     assert.ok(kohde, `${id}: tietuetta ei löydy`);
     const kuvat = kohteenKuvalista(kohde);
@@ -19,6 +19,7 @@ test('neljän CZE-kohteen karusellissa havainne on ennen aitoa valokuvaa', () =>
     assert.equal(kuvat[0].osoite, `https://media.matkakirja.app/karttanostot/20260912/${havainne}`);
     assert.equal(kuvat[1].osoite, `https://media.matkakirja.app/karttanostot/20260912/${valokuva}`);
     assert.match(kuvat[0].lahde, /^Matkakirjan havainnekuva/);
+    assert.match(kuvat[0].tekija, new RegExp(`alkuperäisvalokuva: ${alkuperaistekija.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}`));
     assert.match(kuvat[1].lahde, /^Valokuva:/);
     assert.equal(new Set(kuvat.map((x) => x.osoite)).size, 2);
   }
