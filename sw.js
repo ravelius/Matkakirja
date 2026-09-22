70de803c877183185dd5b0135b814db14378ac62
Sami Reivinen <samireivinen@SamiMacStudio2.localdomain>
2026-09-22 11:48:23 +0300

    v2093: kytke C3-kohtaamiskuvat tavallisiin visoihin

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Palvelutyöntekijä: pelin tiedostot välimuistiin, jotta sovellus toimii myös offline.
-const CACHE = 'matkakirja-2026-09-21.2092';
+const CACHE = 'matkakirja-2026-09-21.2093';
 const SHELL = [
   './',
   './index.html',
