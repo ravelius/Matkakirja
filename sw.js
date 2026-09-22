d79821ba3f085ffb11da2ee16c65220343f132f8
Sami Reivinen <samireivinen@SamiMacStudio2.localdomain>
2026-09-22 13:10:58 +0300

    Kytke C4-C7-kohtaamiskuvat tavallisiin visoihin

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Palvelutyöntekijä: pelin tiedostot välimuistiin, jotta sovellus toimii myös offline.
-const CACHE = 'matkakirja-2026-09-21.2099';
+const CACHE = 'matkakirja-2026-09-21.2100';
 const SHELL = [
   './',
   './index.html',
