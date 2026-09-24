8c88f63472d744accc792ae7001f275b5fb2aa57
ravelius <sami@valokuvaamoklik.fi>
2026-09-24 11:29:19 +0300

    v2155: Kaupunkiliuska pysyy taas merkin korkeud… (v2168) (#3014)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Palvelutyöntekijä: pelin tiedostot välimuistiin, jotta sovellus toimii myös offline.
-const CACHE = 'matkakirja-2026-09-21.2167';
+const CACHE = 'matkakirja-2026-09-21.2168';
 const SHELL = [
   './',
   './index.html',
