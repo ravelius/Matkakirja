0908cc71fa5cfa16a9526cd459af62c19418cf4a
ravelius <sami@valokuvaamoklik.fi>
2026-09-25 15:34:27 +0300

    Sisältöpaketti 1.42: maakuntarajat kaikista web… (v2225) (#3185)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Palvelutyöntekijä: pelin tiedostot välimuistiin, jotta sovellus toimii myös offline.
-const CACHE = 'matkakirja-2026-09-21.2224';
+const CACHE = 'matkakirja-2026-09-21.2225';
 const SHELL = [
   './',
   './index.html',
