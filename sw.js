c3aebe7cc1644491c7bbb22b1773128c83c1684c
ravelius <sami@valokuvaamoklik.fi>
2026-09-26 11:42:26 +0300

    Maakunnat: FIN/EST/SVN pitka-tekstit (v2267) (#3313)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Palvelutyöntekijä: pelin tiedostot välimuistiin, jotta sovellus toimii myös offline.
-const CACHE = 'matkakirja-2026-09-21.2266';
+const CACHE = 'matkakirja-2026-09-21.2267';
 const SHELL = [
   './',
   './index.html',
