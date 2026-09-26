b5040a4e89e1f86ce0b6bbb24d75e86da967bdd6
ravelius <sami@valokuvaamoklik.fi>
2026-09-26 06:15:46 +0300

    Miniatyyrien värikorjaus (v2254) (#3296)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Palvelutyöntekijä: pelin tiedostot välimuistiin, jotta sovellus toimii myös offline.
-const CACHE = 'matkakirja-2026-09-21.2253';
+const CACHE = 'matkakirja-2026-09-21.2254';
 const SHELL = [
   './',
   './index.html',
